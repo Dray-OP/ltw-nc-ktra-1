d7227314a72a41621f2c9b566466b90c75e14fb0
Dray-OP <62054995+Dray-OP@users.noreply.github.com>
2021-09-07 10:09:30 +0700

    first commit

---
+++ About.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="baitap1.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+  
+    
+
+</asp:Content>
---
+++ Login.aspx.cs
@@ -0,0 +1,53 @@
+﻿using baitap1.Model;
+using Microsoft.Build.Tasks.Deployment.Bootstrapper;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace baitap1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+       
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            int length = args.Value.Length;
+            if (length >= 8 && length <= 15)
+                args.IsValid = true;
+            else
+                args.IsValid = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           
+            if (Page.IsValid)
+            {
+                Session["username"] = txt_UserName.Text;
+                Response.Write("Đăng Nhập thành công");
+                Response.Write("Xin chào," + Session["username"]);
+                Response.Redirect("Default.aspx");
+            }
+            else
+            {
+                lblmsg.Text = "Thất bại";
+                
+            }
+        }
+
+        protected void txt_UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
